da564773ae05dc116d73c07a742714f0baef1d59
Washington Antunes <washington.cead@gmail.com>
2023-07-09 17:22:42 -0300

    buscando as views do layout na Activity

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -2,7 +2,6 @@ package br.com.alura.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,5 +11,11 @@ class MainActivity: Activity() {
         super.onCreate(savedInstanceState)
         Toast.makeText(this, "Bem Vindo ao Orgs!", Toast.LENGTH_SHORT).show()
         setContentView(R.layout.activity_main)
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de frutas"
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, manga e maçã"
+        var valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19.99"
     }
 }
