4a9f22e524a77badcc5fc4d60abc1bd808afb37b
Washington Antunes <washington.cead@gmail.com>
2023-07-09 18:27:11 -0300

    finalizando a implementação do adapter

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -3,23 +3,27 @@ package br.com.alura.orgs.ui.activity
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
+import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
-class MainActivity: Activity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Toast.makeText(this, "Bem Vindo ao Orgs!", Toast.LENGTH_SHORT).show()
         setContentView(R.layout.activity_main)
-//        val nome = findViewById<TextView>(R.id.nome)
-//        nome.text = "Cesta de frutas"
-//        val descricao = findViewById<TextView>(R.id.descricao)
-//        descricao.text = "Laranja, manga e maçã"
-//        var valor = findViewById<TextView>(R.id.valor)
-//        valor.text = "19.99"
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutosAdapter(listOf())
+        recyclerView.adapter = ListaProdutosAdapter(
+            context = this, produtos = listOf(
+                Produto(nome = "teste", descricao = "teste desc", valor = BigDecimal("19.99")),
+                Produto(nome = "teste 2", descricao = "teste desc 2", valor = BigDecimal("29.99")),
+                Produto(nome = "teste 3", descricao = "teste desc 3", valor = BigDecimal("39.99"))
+            )
+        )
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
