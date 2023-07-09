08892dd0e66ba1503b1c6662879743f30f6d939e
Washington Antunes <washington.cead@gmail.com>
2023-07-09 15:25:55 -0300

    criando a primeira view

--- app/src/main/java/br/com/alura/orgs/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -2,12 +2,17 @@ package br.com.alura.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity: Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem vindo(a) ao Orgs!", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Bem Vindo ao Orgs!", Toast.LENGTH_SHORT).show()
+        val view = TextView(this)
+        view.setText("cesta de frutas")
+        setContentView(view)
     }
 }
